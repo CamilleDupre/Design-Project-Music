1a8a3ebbbdfcf06885bceb7ec33d6f28c5cdb6ab
CamilleDupre <47179915+CamilleDupre@users.noreply.github.com>
2022-01-16 16:27:03 +0100

    1st commit

---
+++ Assets/EliseScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EliseScript : MonoBehaviour
+{
+    public void PressPiano()
+    {
+        Debug.Log("WIN !!!");
+    }
+}
