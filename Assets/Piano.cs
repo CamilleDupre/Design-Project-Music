9e7f244fd747fd9c2f657cbf2d3ab6720b1973d2
CamilleDupre <47179915+CamilleDupre@users.noreply.github.com>
2022-01-17 14:38:47 +0100

    Prefab piano + script piano

---
+++ Assets/Piano.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piano : MonoBehaviour
+{
+    public void PressDo()
+    {
+        Debug.Log("Do");
+    }
+    public void PressRe()
+    {
+        Debug.Log("Re");
+    }
+    public void PressMi()
+    {
+        Debug.Log("Mi");
+    }
+    public void PressFa()
+    {
+        Debug.Log("Fa");
+    }
+    public void PressSol()
+    {
+        Debug.Log("Sol");
+    }
+    public void PressLa()
+    {
+        Debug.Log("La");
+    }
+    public void PressSi()
+    {
+        Debug.Log("Si");
+    }
+    public void PressB1()
+    {
+        Debug.Log("B1");
+    }
+    public void PressB2()
+    {
+        Debug.Log("B2");
+    }
+    public void PressB3()
+    {
+        Debug.Log("B3");
+    }
+    public void PressB4()
+    {
+        Debug.Log("B4");
+    }
+    public void PressB5()
+    {
+        Debug.Log("B5");
+    }
+}
