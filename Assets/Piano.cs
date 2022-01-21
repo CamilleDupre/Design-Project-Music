8ea0ade613df74223b9b36f6aad13789eabebe80
CamilleDupre <47179915+CamilleDupre@users.noreply.github.com>
2022-01-21 18:51:23 +0100

    practice

--- Assets/Piano.cs
+++ Assets/Piano.cs
@@ -1,36 +1,123 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Piano : MonoBehaviour
 {
+    public PressButton Dokey;
+    public PressButton Rekey;
+    public PressButton Mikey;
+    public PressButton Fakey;
+    public PressButton Solkey;
+    public PressButton Lakey;
+    public PressButton Sikey;
+
+    public Button donote ;
+    public Button re ;
+    public Button mi ;
+    public Button fa ;    
+    public Button sol ;
+    public Button la ;
+    public Button si ;
+
+    public Text text;
+
+    void Update()
+    {
+        /*
+        donote.GetComponent<Image>().color = Color.black;
+        re.GetComponent<Image>().color = Color.black;
+        mi.GetComponent<Image>().color = Color.black;
+        fa.GetComponent<Image>().color = Color.black;
+        sol.GetComponent<Image>().color = Color.black;
+        la.GetComponent<Image>().color = Color.black;
+        si.GetComponent<Image>().color = Color.black;
+        */
+        
+        if (Dokey.PublicIsPressed())
+        {
+            donote.GetComponent<Image>().color = Color.green;
+            text.text = "Note : DO";
+        }
+
+       else if (Rekey.PublicIsPressed())
+        {
+            re.GetComponent<Image>().color = Color.green;
+            text.text = "Note : RE";
+        }
+
+        else if (Mikey.PublicIsPressed())
+        {
+            mi.GetComponent<Image>().color = Color.green;
+            text.text = "Note : MI";
+        }
+
+        else if (Fakey.PublicIsPressed())
+        {
+            fa.GetComponent<Image>().color = Color.green;
+            text.text = "Note : FA";
+        }
+
+        else if (Solkey.PublicIsPressed())
+        {
+            sol.GetComponent<Image>().color = Color.green;
+            text.text = "Note : SOL";
+        }
+
+       else  if (Lakey.PublicIsPressed())
+        {
+            la.GetComponent<Image>().color = Color.green;
+            text.text = "Note : LA";
+        }
+
+       else if (Sikey.PublicIsPressed())
+        {
+            si.GetComponent<Image>().color = Color.green;
+            text.text = "Note : SI";
+        }
+        else
+        {
+            text.text = "Note :";
+        }
+
+    }
+
+
     public void PressDo()
     {
         Debug.Log("Do");
+        donote.GetComponent<Image>().color = Color.black;
     }
     public void PressRe()
     {
         Debug.Log("Re");
+        re.GetComponent<Image>().color = Color.black;
     }
     public void PressMi()
     {
         Debug.Log("Mi");
+        mi.GetComponent<Image>().color = Color.black;
     }
     public void PressFa()
     {
         Debug.Log("Fa");
+        fa.GetComponent<Image>().color = Color.black;
     }
     public void PressSol()
     {
         Debug.Log("Sol");
+        sol.GetComponent<Image>().color = Color.black;
     }
     public void PressLa()
     {
         Debug.Log("La");
+        la.GetComponent<Image>().color = Color.black;
     }
     public void PressSi()
     {
         Debug.Log("Si");
+        si.GetComponent<Image>().color = Color.black;
     }
     public void PressB1()
     {
---
+++ Assets/PressButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PressButton : Button
+ {
+        public bool PublicIsPressed()
+        {
+            return base.IsPressed();
+        }
+ }
+    
